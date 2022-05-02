1c38f24e13e3ce5012db791b52e00c171e1cd158
State Andrei Sergiu <state.andrei.sergiu@gmail.com>
2022-05-02 20:32:42 +0300

    v4

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/*let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+
+if (hasDriversLicense) console.log(`I can drive!`);
+
+const calcAge = birthYear => {
+    return 2037 - birthYear;
+}
+console.log(calcAge(2002));*/
+
+// console.log('asd'.includes('d'));
+
+// const input = prompt(`Shakira?`);
+
+//Challenge
