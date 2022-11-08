35283e0eaf131d72067f5982aa2890a378689867
State Andrei Sergiu <state.andrei.sergiu@gmail.com>
2022-11-08 21:24:10 +0200

    v10.129

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,55 @@
 'use strict';
+
+// const bookings = [];
+
+// const createBooking = function (
+// 	flightNum,
+// 	numPassengers = 1,
+// 	price = 199 * numPassengers
+// ) {
+// 	// ES5
+// 	// numPassengers = numPassengers || 1;
+// 	// price = price || 199;
+// 	const booking = {
+// 		flightNum,
+// 		numPassengers,
+// 		price,
+// 	};
+// 	console.log(booking);
+// 	bookings.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', 2);
+// createBooking('LH123', 5);
+
+// createBooking('LH123', undefined, 1000);
+
+const flight = 'LH234';
+const jonas = {
+	name: 'Jonas',
+	passport: 2343243535,
+};
+
+const checkIn = function (flightNum, passenger) {
+	flightNum = 'LH999';
+	passenger.name = 'Mr. ' + passenger.name;
+
+	if (passenger.passport === 2343243535) {
+		alert('Check In');
+	} else {
+		alert('Wrong passport!');
+	}
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+const newPassport = function (person) {
+	person.passport = Math.trunc(Math.random() * 100000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
